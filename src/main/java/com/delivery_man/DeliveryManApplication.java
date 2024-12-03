08715d188bb52cb59a7501ff60bfa7b45b757682
이자드 <kimminkyeong0418@gmail.com>
2024-12-03 16:02:43 +0900

    build: 프로젝트 기본 세팅

---
+++ src/main/java/com/delivery_man/DeliveryManApplication.java
@@ -0,0 +1,13 @@
+package com.delivery_man;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryManApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryManApplication.class, args);
+	}
+
+}
