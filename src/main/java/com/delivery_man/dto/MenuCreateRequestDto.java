20bfb17b3d4528d5e023b643b094afe436dd4122
이자드 <kimminkyeong0418@gmail.com>
2024-12-03 22:19:48 +0900

    feat: menu 생성 기능 구현

---
+++ src/main/java/com/delivery_man/dto/MenuCreateRequestDto.java
@@ -0,0 +1,30 @@
+package com.delivery_man.dto;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Null;
+import jakarta.validation.constraints.Positive;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@NoArgsConstructor
+@Getter
+public class MenuCreateRequestDto {
+    @Null
+    private Long userId;
+    @Null
+    private Long shopId;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    @Positive
+    private BigDecimal price;
+
+    public void setIds(Long userId,Long shopId){
+        this.userId=userId;
+        this.shopId=shopId;
+    }
+}
--- src/main/java/com/delivery_man/entity/Menu.java
+++ src/main/java/com/delivery_man/entity/Menu.java
@@ -1,12 +1,15 @@
 package com.delivery_man.entity;
 
+import com.delivery_man.dto.MenuCreateRequestDto;
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Getter
 @Entity
+@NoArgsConstructor
 public class Menu extends BaseEntity {
     @Id
     @Column(name = "menu_id")
@@ -21,4 +24,9 @@ public class Menu extends BaseEntity {
     private String status;
 
 
+    public Menu(MenuCreateRequestDto dto) {
+        this.name = dto.getName();
+        this.price = dto.getPrice();
+        this.status="use";
+    }
 }
