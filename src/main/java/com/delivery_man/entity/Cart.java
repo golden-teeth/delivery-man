785d7af698458e4d061e14e096343fbaeeaf11b3
sumyeom <wjstndus75@naver.com>
2024-12-05 19:59:07 +0900

    fix: Cart 엔티티 CreateAndUpdateDateEntity 로 변경
    1. Cart 엔티티 CreateAndUpdateDateEntity 로 변경

--- src/main/java/com/delivery_man/entity/Cart.java
+++ src/main/java/com/delivery_man/entity/Cart.java
@@ -8,7 +8,7 @@ import lombok.NoArgsConstructor;
 @Table
 @Getter
 @NoArgsConstructor
-public class Cart extends BaseEntity{
+public class Cart extends CreateAndUpdateDateEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
