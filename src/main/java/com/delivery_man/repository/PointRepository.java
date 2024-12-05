c6be9238158d9582ff7fa1a776ae79cd29faa2ee
이자드 <kimminkyeong0418@gmail.com>
2024-12-05 16:05:26 +0900

    feat: 만료된 point 삭제 기능 추가

--- src/main/java/com/delivery_man/repository/PointRepository.java
+++ src/main/java/com/delivery_man/repository/PointRepository.java
@@ -3,7 +3,13 @@ package com.delivery_man.repository;
 import com.delivery_man.entity.Point;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
 
 @Repository
 public interface PointRepository extends JpaRepository<Point, Long> {
+
+    @Transactional
+    void deletePointByCreatedAtLessThan(LocalDateTime cutoffDateTime);
 }
