a50d6d5be997ab647eae38d2eaba32d80d7281b5
sumyeom <wjstndus75@naver.com>
2024-12-06 14:21:03 +0900

    feat: 광고 신청 상태에 따른 가게 조회 리스트 수정
    1. 광고가 APPLY 상태일 때 먼저 조회되고 광고가 승인 상태가 아닌 경우와 광고 신청을 안한 경우에도 리스트 출력되도록 수정

--- src/main/java/com/delivery_man/constant/AdErrorCode.java
+++ src/main/java/com/delivery_man/constant/AdErrorCode.java
@@ -8,8 +8,10 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum AdErrorCode implements ErrorCode {
     /* 404 찾을 수 없음 */
-    AD_NOT_FOUND(HttpStatus.NOT_FOUND, "광고 신청 건을 찾을 수 없습니다"),;
+    AD_NOT_FOUND(HttpStatus.NOT_FOUND, "광고 신청 건을 찾을 수 없습니다"),
 
+    /* 401 권한 없음 */
+    INVALID_SHOP_AD(HttpStatus.UNAUTHORIZED, "본인의 가게에만 광고 신청 및 조회를 할 수 있습니다."),;
     private final HttpStatus httpStatus;
     private final String message;
 }
